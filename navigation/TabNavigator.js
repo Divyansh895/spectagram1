a45dffa45f5f2e0f130bee50b66d742d8e55a5ad
Divyansh Patel <divyansh234patel@gmail.com>
2024-04-03 16:01:19 +0530

    first commit

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -3,6 +3,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Feed from "../screens/Feed";
 import CreatePost from "../screens/CreatePost";
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
 const Tab = createBottomTabNavigator();
 
@@ -34,3 +35,38 @@ const BottomTabNavigator = () => {
 }
 
 export default BottomTabNavigator
+
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:"black"
+    },
+    droidSafeArea:{
+        marginTop:Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+    },
+    appTitle:{
+        flex:0.07,
+        flexDirection:"row"
+    },
+    appIcon:{
+        flex:0.2,
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    iconImage:{
+        width:"100%",
+        height:"100%",
+        resizeModel:"contain"
+    },
+    appTitleTextContiner:{
+        flex:0.8,
+        justifyContent:"center",
+    },
+    appTitleText:{
+        color:"white",
+        fontSize:RFValue(28)
+    },
+    cardContainer:{
+        flex:0.85,
+    }
+})
